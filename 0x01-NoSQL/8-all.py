b0db21e6a1419e3e1104f2c16eee92116f64f488
AltechInferno <altechinferno@gmail.com>
2024-01-24 00:51:44 +0000

    tasks

---
+++ 0x01-NoSQL/8-all.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+'''func lists all documents in a collection
+'''
+
+
+def list_all(mongo_collection):
+    '''lists all documents in a collection
+    '''
+    return [doc for doc in mongo_collection.find()]
